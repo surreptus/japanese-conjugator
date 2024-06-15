8ba9b58ec3fc117fdd27e1e3f6bef31440106628
surreptus <47543507+surreptus@users.noreply.github.com>
2024-06-15 15:29:53 -0700

    test: remove te cases

--- src/__tests__/inflect.test.ts
+++ src/__tests__/inflect.test.ts
@@ -3,6 +3,8 @@ import { inflect } from "../inflect";
 import { describe, it, expect } from "@jest/globals";
 
 describe("inflect", () => {
+  const verb = "飲む";
+
   it("it should throw on an unknown group", () => {
     expect(() => inflect("foo", "error" as unknown as Inflection)).toThrow(
       "invalid inflection provided, or not yet implemented!"
@@ -23,59 +25,31 @@ describe("inflect", () => {
     expect(inflect("知る", Inflection.Causative)).toBe("知らせる");
   });
 
-  /**
-   * these its aren't meant to be exhaustive, but to ensure that the correct case
-   * in the switch statemenet is used. the actual it for each of the stems / forms are
-   * captured in their own its
-   */
-  describe("godan", () => {
-    const verb = "飲む";
-
-    it("it should return the past form", () => {
-      expect(inflect(verb, Inflection.NonPast)).toBe("飲む");
-    });
-
-    it("it should return the non-past form", () => {
-      expect(inflect(verb, Inflection.Past)).toBe("飲んだ");
-    });
-
-    it("it should return the non-past polite form", () => {
-      expect(inflect(verb, Inflection.NonPastPolite)).toBe("飲みます");
-    });
-
-    it("it should return the past polite form", () => {
-      expect(inflect(verb, Inflection.PastPolite)).toBe("飲みました");
-    });
-
-    it("it should return the passive form", () => {
-      expect(inflect(verb, Inflection.Passive)).toBe("飲まれる");
-    });
-
-    it("it should return the te form", () => {
-      const cases = [
-        ["飲む", "飲んで"],
-        ["死ぬ", "死んで"],
-        ["待つ", "待って"],
-        ["泳ぐ", "泳いで"],
-        ["書く", "書いて"],
-        ["買う", "買って"],
-        // ["走る", "走って"], fix this
-        ["消す", "消して"],
-      ];
-
-      cases.forEach(([input, expected]) => {
-        expect(inflect(input, Inflection.Te)).toBe(expected);
-      });
-    });
-
-    it("it should return the potential form", () => {
-      expect(inflect(verb, Inflection.Potential)).toBe("飲める");
-    });
+  it("it should return the past form", () => {
+    expect(inflect(verb, Inflection.NonPast)).toBe("飲む");
   });
 
-  describe("ichidan", () => {
-    it("it should return the non-past form", () => {
-      expect(inflect("上げる", Inflection.NonPast)).toBe("上げる");
-    });
+  it("it should return the non-past form", () => {
+    expect(inflect(verb, Inflection.Past)).toBe("飲んだ");
+  });
+
+  it("it should return the non-past polite form", () => {
+    expect(inflect(verb, Inflection.NonPastPolite)).toBe("飲みます");
+  });
+
+  it("it should return the past polite form", () => {
+    expect(inflect(verb, Inflection.PastPolite)).toBe("飲みました");
+  });
+
+  it("it should return the passive form", () => {
+    expect(inflect(verb, Inflection.Passive)).toBe("飲まれる");
+  });
+
+  it("it should return the te form", () => {
+    expect(inflect(verb, Inflection.Te)).toBe("飲んで");
+  });
+
+  it("it should return the potential form", () => {
+    expect(inflect(verb, Inflection.Potential)).toBe("飲める");
   });
 });
